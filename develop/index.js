bf4fcd31d3d44839df387ec7ac70efb665c0eb76
DimitriBoldon <dimitri@tiksdd.com>
2023-07-19 06:49:21 -0500

    edited files

--- develop/index.js
+++ develop/index.js
@@ -40,7 +40,7 @@ function promptUser() {
       });
   }
   
-  promptUser();
+  
 
 
 function writeToFile(fileName, answers) {
@@ -50,4 +50,31 @@ function writeToFile(fileName, answers) {
     svgString += "<g>";
     
     svgString += `${answers.shape}`;
-}
+
+
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+      shapeChoice = new Triangle();
+      svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+      shapeChoice = new Square();
+      svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+      shapeChoice = new Circle();
+      svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+  
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+  
+    fs.writeFile(fileName, svgString, (err) => {
+      err ? console.log(err) : console.log("Generated logo.svg");
+    });
+  }
+
+
+
+
+promptUser();
+
